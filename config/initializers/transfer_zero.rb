bc2ee0a789914b36b289b814ea6331dba7fdf482
Sylvance <kerandisylvance@gmail.com>
2019-10-24 02:56:55 +0300

    Add tz sdk

---
+++ config/initializers/transfer_zero.rb
@@ -0,0 +1,5 @@
+TransferZero.configure do |config|
+    config.api_key = 'xdwLSvQ1R6LlW+DWlAq5Lc/pfxmnu6qxmAyyAEa/IQ4jb1uQrqJkfNd89ogHtN9/YW0BwsQ3fjGZ1j+Cm7M05A=='
+    config.api_secret = 'VTcCZPNyoWcQJ/W+CuisldCHaLnv/8txbHCEb3/bkQ9kyUUd3V1UXVD26AXK+yBIlHQm6dmi0QM2r3IbRnBc3w=='
+    config.host = 'https://api-sandbox.transferzero.com/v1'
+end
