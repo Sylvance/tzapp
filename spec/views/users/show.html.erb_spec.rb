a9464563018e88e9e562f77adedb64ebc86e5e37
Sylvance <kerandisylvance@gmail.com>
2019-10-24 03:19:24 +0300

    Add user scaffold

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", type: :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+      :name => "Name",
+      :bio => "MyText",
+      :balance => 2
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/2/)
+  end
+end
